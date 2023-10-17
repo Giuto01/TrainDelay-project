96c957f671e8034dfb2579e2fa4af406fed8ca1f
Vito <vitoproscia.1704@gmail.com>
2023-10-17 17:58:27 +0200

    Build ML model, UI adjustment

--- options.py
+++ options.py
@@ -1,10 +1,10 @@
 import networkx as nx
-import re 
+import re
 from knowledge_base import knowledge_engine
 from prettytable import PrettyTable
+import utils
 
-
-def search(departure, arrival, time):
+def search(departure, arrival, time, model):
     '''
     Search for trains between two stations for a given departure time.
 
@@ -18,36 +18,45 @@ def search(departure, arrival, time):
         or None if no trains are available.
     '''
 
-    engine = knowledge_engine.PrologEngine("knowledge_base/trenitalia_schedule.pl", "knowledge_base/rules.pl","knowledge_base/stations.pl")
+    engine = knowledge_engine.PrologEngine(
+        "knowledge_base/trenitalia_schedule.pl", "knowledge_base/rules.pl", "knowledge_base/stations.pl")
+    # Get train list for knowledge base
+    trainsList = engine.trains_departure_between_stations_name_at_time(
+        departure, arrival, time)
 
-    trainsList = engine.trains_departure_between_stations_name_at_time(departure, arrival, time)
-
-    # Creazione dell'oggetto PrettyTable
     table = PrettyTable()
     table.border = True
     table.padding_width = 3
-    # Definizione delle colonne
-    table.field_names = ["TrainID", "TrainType", "DepartureStation", "ArrivalStation", "DepartureTime", "ArrivalTime"]
+    table.field_names = ["TrainID", "TrainType", "DepartureStation",
+                         "ArrivalStation", "DepartureTime", "ArrivalTime", "Predicted delay (AI)"]
     table.sortby = "ArrivalTime"
-    table.title = "\033[1m"+ str(departure) + " --> " + str(arrival) + "\033[0m"
+    table.title = "\033[1m" + str(departure) + \
+        " --> " + str(arrival) + "\033[0m"
 
     if len(trainsList) != 0:
-    
+
         for train in trainsList:
             trainInfo = engine.get_train_fact_by_ID(train)
             trainID = train
             trainType = trainInfo["Type"]
-            departureStationName = re.sub("(b|')", "", str(engine.station_name_by_ID(trainInfo["DepartureID"])))
-            arrivalStationName = re.sub("(b|')", "", str(engine.station_name_by_ID(trainInfo["ArrivalID"])))
+            departureStationName = re.sub("(b|')", "", str(
+                engine.station_name_by_ID(trainInfo["DepartureID"])))
+            arrivalStationName = re.sub("(b|')", "", str(
+                engine.station_name_by_ID(trainInfo["ArrivalID"])))
             departureTime = trainInfo["DepartureTime"]
             arrivalTime = trainInfo["ArrivalTime"]
-            table.add_row([trainID, trainType, departureStationName, arrivalStationName, departureTime, arrivalTime], divider=True)
+            # Make delay prediction
+            prediction = utils.prediction_str(model.predict([[trainID, utils.time_to_minutes(
+                departureTime), utils.time_to_minutes(arrivalTime), utils.bin_train_type(trainType)]]))
+            table.add_row([trainID, trainType, departureStationName, arrivalStationName,
+                          departureTime, arrivalTime, prediction], divider=True)
 
         return table
     else:
         return None
 
-def searchItinerary(graph, departure, arrival):
+
+def searchItinerary(graph, departure, arrival, model):
     '''
     Search for the shortest itinerary between two stations using a railway connections graph.
 
@@ -60,37 +69,50 @@ def searchItinerary(graph, departure, arrival):
         None: The function prints tables with details of trains for each leg of the journey.
     '''
 
-    engine = knowledge_engine.PrologEngine("knowledge_base/trenitalia_schedule.pl", "knowledge_base/rules.pl","knowledge_base/stations.pl")
+    engine = knowledge_engine.PrologEngine(
+        "knowledge_base/trenitalia_schedule.pl", "knowledge_base/rules.pl", "knowledge_base/stations.pl")
 
-    departureID = engine.station_ID_by_name(departure)
-    arrivalID = engine.station_ID_by_name(arrival)
+    try:
+        departureID = engine.station_ID_by_name(departure)
+        arrivalID = engine.station_ID_by_name(arrival)
+        # Search path between stations
+        path = nx.shortest_path(graph, departureID, arrivalID)
 
-    path = nx.shortest_path(graph, departureID, arrivalID)
+        if len(path) != 0 and path[0] == departureID:
 
-    if len(path) != 0 and path[0] == departureID:
-            
             try:
                 for index, station in enumerate(path):
-                    station_name = re.sub("(b|')", "", str(engine.station_name_by_ID(station)))
-                    next_station_name = re.sub("(b|')", "", str(engine.station_name_by_ID(path[index+1])))
-
-                    trains = engine.trains_departure_between_stations_name(station_name, next_station_name)
+                    station_name = re.sub("(b|')", "", str(
+                        engine.station_name_by_ID(station)))
+                    next_station_name = re.sub("(b|')", "", str(
+                        engine.station_name_by_ID(path[index+1])))
+                    # Get train list for knowledge base
+                    trains = engine.trains_departure_between_stations_name(
+                        station_name, next_station_name)
                     if len(trains) != 0:
-                        # Creazione dell'oggetto PrettyTable
                         table = PrettyTable()
                         table.border = True
                         table.padding_width = 3
-                        # Definizione delle colonne
-                        table.field_names = ["TrainID", "DepartureTime", "ArrivalTime"]
+                        table.field_names = [
+                            "TrainID", "DepartureTime", "ArrivalTime", "Predicted delay (AI)"]
                         table.sortby = "DepartureTime"
-                        table.title = "\033[1m"+ station_name + " --> " + next_station_name + "\033[0m"
+                        table.title = "\033[1m" + station_name + \
+                            " --> " + next_station_name + "\033[0m"
                         for trainID in trains:
                             tr = engine.get_train_fact_by_ID(trainID)
                             departureTime = tr["DepartureTime"]
                             arrivalTime = tr["ArrivalTime"]
-                            table.add_row([trainID, departureTime, arrivalTime], divider=True)
+                            trainType = tr["Type"]
+                            if (departureTime == "none" or arrivalTime == "none"):
+                                prediction = "Null"
+                            else:
+                                # Make delay prediction
+                                prediction = utils.prediction_str(model.predict([[trainID, utils.time_to_minutes(
+                                    departureTime), utils.time_to_minutes(arrivalTime), utils.bin_train_type(trainType)]]))
+                            table.add_row(
+                                [trainID, departureTime, arrivalTime, prediction], divider=True)
                         print(table)
             except:
                 pass
-    else:
+    except:
         print("\nNessun itineriario disponibile per la tua ricerca. 😥\n")
---
+++ utils.py
@@ -0,0 +1,59 @@
+def time_to_minutes(time):
+    '''
+    Converts a time string in the format "HH:MM" to the total number of minutes since midnight.
+
+    Args:
+        time (str): A time string in the "HH:MM" format.
+
+    Returns:
+        int: The total minutes since midnight.
+
+    Example:
+        >>> time_to_minutes("08:30")
+        510
+    '''
+
+    hour, minutes = time.split(":")
+    return int(hour) * 60 + int(minutes)
+
+
+def bin_train_type(trainType):
+    '''
+    Maps a train type string to a binary value.
+
+    Args:
+        trainType (str): The train type, which can be "regionale" or "nazionale".
+
+    Returns:
+        int or None: 1 if trainType is "regionale," 0 if trainType is "nazionale," or None if not recognized.
+
+    Example:
+        >>> bin_train_type("regionale")
+        1
+    '''
+
+    if trainType == "regionale":
+        return 1
+    elif trainType == "nazionale":
+        return 0
+
+
+def prediction_str(pred):
+    '''
+    Converts a binary prediction result to a human-readable string.
+
+    Args:
+        pred (int): Binary prediction result (0 or 1).
+
+    Returns:
+        str: "Yes" if pred is 1, "No" if pred is 0.
+
+    Example:
+        >>> prediction_str(1)
+        "Yes"
+    '''
+
+    if pred[0] == 0:
+        return "No"
+    else:
+        return "Yes"
